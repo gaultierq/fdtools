a10dfffa9b5c5b50f39093658ecae572f03f24c5
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-10-10 14:37:09 +0300

    shell PWD

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PWD="$(dirname $(readlink -f $0))"
 
-echo "updating backend" >> $PWD/deploy.log
+echo "updating backend from $PWD" >> $PWD/deploy.log
 $PWD/deploy_updated_backend.sh >> $PWD/deploy.log 2>&1 &
