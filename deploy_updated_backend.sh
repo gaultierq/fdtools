98470b44833d6687385cfda631c30ccd48a8556e
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-10-10 14:09:40 +0300

    fix typo in update

--- deploy_updated_backend.sh
+++ deploy_updated_backend.sh
@@ -5,7 +5,7 @@ docker pull registry.gitlab.com/clicknfid/fdbot:latest &&
 docker rm -f fdbot ;
 
 # migration
-docker run --rm fdbot registry.gitlab.com/clicknfid/fdbot ./scripts/migrate.js &&
+docker run --rm registry.gitlab.com/clicknfid/fdbot ./scripts/migrate.js &&
 
 # deployment
 docker run --name fdbot -p 80:4000 -d registry.gitlab.com/clicknfid/fdbot
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "updating backend" >> $PWD/deploy.log
 $PWD/deploy_updated_backend.sh >> $PWD/deploy.log &2>1 &
