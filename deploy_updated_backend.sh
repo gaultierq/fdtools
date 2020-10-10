a02453c2298d203609eaee9d91b7a4a6c64aa1ee
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-10-10 14:13:31 +0300

    deploy ++

--- deploy_updated_backend.sh
+++ deploy_updated_backend.sh
@@ -4,8 +4,9 @@ docker pull registry.gitlab.com/clicknfid/fdbot:latest &&
 
 docker rm -f fdbot ;
 
+echo "Starting migration"
 # migration
-docker run --rm registry.gitlab.com/clicknfid/fdbot ./scripts/migrate.js &&
+docker run --rm registry.gitlab.com/clicknfid/fdbot ./scripts/migrate.js && echo "migration ok" &&
 
 # deployment
-docker run --name fdbot -p 80:4000 -d registry.gitlab.com/clicknfid/fdbot
+docker run --name fdbot -p 80:4000 -d registry.gitlab.com/clicknfid/fdbot && echo "deployment ok"
--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "updating backend" >> $PWD/deploy.log
-$PWD/deploy_updated_backend.sh >> $PWD/deploy.log &2>1 &
+$PWD/deploy_updated_backend.sh >> $PWD/deploy.log 2>&1 &
