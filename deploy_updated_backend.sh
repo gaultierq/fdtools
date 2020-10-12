b6501ae6e853b65b0834dfbad18e5bb3fdcf03bf
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-10-12 08:03:49 +0300

    docker prune

--- deploy_updated_backend.sh
+++ deploy_updated_backend.sh
@@ -9,4 +9,7 @@ echo "Starting migration"
 docker run --rm registry.gitlab.com/clicknfid/fdbot ./scripts/migrate.js && echo "migration ok" &&
 
 # deployment
-docker run --name fdbot -p 80:4000 -d registry.gitlab.com/clicknfid/fdbot && echo "deployment ok"
+docker run --name fdbot -p 80:4000 -d registry.gitlab.com/clicknfid/fdbot && echo "deployment ok" &&
+
+# cleaning up
+docker system prune --force && echo "prune ok"
